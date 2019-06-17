aa1de2d50bf323104f346f9d40b4dc80a4b054f4
nekot <nekot0912@gmai.com>
2019-06-17 11:06:55 +0800

    pub

--- IGGTaipeRD2/PubApi.php
+++ IGGTaipeRD2/PubApi.php
@@ -261,7 +261,7 @@
 					$color="#444444";
 				    $pic="Outsourcing/pic/".$outTmp[$i][13];
 					$outName=substr($outTmp[$i][2],0,7);
-					$color=getColor($outTmp[$i][7]);
+					$color=getTypeColor($outTmp[$i][7]);
 				    DrawMemberLinkRect(	$outName,"9","#ffffff", $x-2, $startY ,"70","40",$color,$outTmp[$i][7],$outTmp[$i][2], $Link,$pic);
 				    $x+=  74;
 			    }
