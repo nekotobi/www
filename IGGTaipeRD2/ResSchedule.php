c6df6b246de0196f02aa3a08439888bcd24568ba
nekot <nekot0912@gmai.com>
2020-01-09 10:19:22 +0800

    vfx

--- IGGTaipeRD2/ResSchedule.php
+++ IGGTaipeRD2/ResSchedule.php
@@ -8,9 +8,11 @@
    include('CalendarApi.php');  
    include('mysqlApi.php');
    include('scheduleApi.php');
-   defineData_schedule();   //定義基礎資料 
+   
+    defineData_schedule();   //定義基礎資料 
     GetCalendarData(); //取得日曆資料(scheduleApi)
     DrawBaseCalendar_v2(); //列印基礎日期資料(scheduleApi)
+    DrawOrders();
 ?>
 <?php //主要資料
  	 function  defineData_schedule(){
@@ -50,8 +52,30 @@
 			    $milestoneSelect=returnArraybySort($mt2,2);
 	 }
 ?>
+<?php //List
+        function DrawOrders(){
+		         global  $MainPlanData;
+				 $typesOrderT= filterArray($MainPlanData,5,"工項");
+				 $typesOrder= filterArray( $typesOrderT,10,"UI");
+                 $Rect=array(20,200,300,20);	
+                 $fontColor="#ffffff";
+		         $BgColor=array("#333333","#000000");			
+                 echo "<div id=LockX style='LockX :pointer ; color:#ffaabb;  z-index:-4; position:fixed ; ";
+				 echo "top:".$Rect[1]."px; left:".$Rect[0]."px; width:".$Rect[2]."px; height:1222px; background-color:#000000; ";
+                // echo "top:".$Rect[1]."px; left:".$Rect[0]."px; width:".$Rect[2]."px;height:133px; background-color:#000000; ' "; 
+				 echo " >aa"  ; 	 
+				 for($i=0;$i<count($typesOrder);$i++){
+				     $a= $i % 2;
+					 $msg=  $typesOrder[$i][3];
+					 $BGc=$BgColor[$a];
+					  DrawLinkRect_LayerPos($msg,10,$fontColor,$Rect, $BGc,$Link,$border,1);
+				     $Rect[1]+=10;
+				 }
+				   echo "</div>";
+		}
+?>
 <?php
-DrawList();
+      
        function DrawList(){
 		   $x=100;
 		   $y=100;
@@ -60,13 +84,21 @@ DrawList();
 		   $Rect=array(100,100,100,1000);
 		   $fontColor="#ffffff";
 		   $BgColor="#000000";
- 
 		   $msg="bbb";
 		   DrawLinkRect_LayerPos($msg,12,$fontColor,$Rect,$BgColor,$Link,$border,-1);
 	   }
 	    function DrawLinkRect_LayerPos($msg,$fontSize,$fontColor,$Rect,$BgColor,$Link ,$Layer){
+	          echo "<div id=LockX2 style='LockX :pointer ; color:".$fontColor."; " ;
+			  echo " z-index:".$Layer ."; ";
+			  echo " text-align:center ; font-weight:bolder ;font-family:Microsoft JhengHei; font-size:".$fontSize."px;";
+			  echo " position:static; ";
+              echo "top:".$Rect[1]."px; left:".$Rect[0]."px; width:".$Rect[2]."px;height:".$Rect[3]."px; background-color:".$BgColor."; ' "; 
+			  echo " onclick=location.href='".$Link."'; >";
+			  echo $msg;
+	          echo "</div>";
+	   }
+	    function DrawLinkRect_LayerPos_bak($msg,$fontSize,$fontColor,$Rect,$BgColor,$Link ,$Layer){
 	          echo "<div id=LockX  style='LockX :pointer ; color:".$fontColor."; " ;
- 
 			  echo " z-index:".$Layer ."; ";
 			  echo " text-align:center ; font-weight:bolder ;font-family:Microsoft JhengHei; font-size:".$fontSize."px;";
 			  echo " position:fixed ; ";
@@ -78,7 +110,7 @@ DrawList();
 	   }
  
 ?>
-
+<body bgcolor="#b5c4b1">
  <script type="text/javascript">
      var a= document.getElementById("LockX");
      a.innerHTML ="xxxx";
