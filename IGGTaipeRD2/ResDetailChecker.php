83694693ab2cc041352155e3f192fbc0b98ee261
qianxinhuang <nekot0912@gmail.com>
2020-07-28 16:33:52 +0800

    22

--- IGGTaipeRD2/ResDetailChecker.php
+++ IGGTaipeRD2/ResDetailChecker.php
@@ -2,15 +2,49 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
    <title>資源詳細檢查表</title>
 </head>
+<body bgcolor="#b5c4b1">
+<script type="text/javascript"> //傳遞變數
+    function post_to_url(path, params, method) {
+    method = method || "post";  
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+    for(var key in params) {
+        var hiddenField = document.createElement("input");
+        hiddenField.setAttribute("type", "hidden");
+        hiddenField.setAttribute("name", key);
+        hiddenField.setAttribute("value", params[key]);
+        form.appendChild(hiddenField);
+    }
+    document.body.appendChild(form);  
+    form.submit();
+}
+</script>
 <?php  //主控台
    include('PubApi.php');
+   
    include('mysqlApi.php');
    setPost();
    DefineDatas();
+   CheckSubmit();
    DrawButtons();
    DrawList();
    DrawChecker();
+   checkResData();
  ?>
+ 
+ <?php  //post
+        function JavaPost($PostArray,$URL){
+			$params="{";
+			for($i=0;$i<count($PostArray);$i++){
+			    $params=$params."'".$PostArray[$i][0]."':'".$PostArray[$i][1]."'";
+		    	if(count($PostArray)>1) $params=$params.",";
+			}
+			$params=$params."}";
+		    $javaCom=  "post_to_url('".$URL."', ".$params.");";
+            echo "<script language='JavaScript'>".$javaCom."</script>"; 
+      }
+?>
  
  <?php //main
  	function setPost(){
@@ -19,9 +53,10 @@
 	}
    function DefineDatas(){
 			global $URL;
-			 global $typeVal;
+			global $typeVal;
 			$URL="ResDetailChecker.php";
 		    global  $data_library,$tableName;
+			 global $ResTable ;
             $ResTable="fpresdata";
 			$tableName="reschecker";
 			$data_library="iggtaiperd2";
@@ -32,7 +67,7 @@
 			if( $typeVal[0][1]=="")return;
 		    $TypeResCheck_T=filterArray($ResChecker,0, $typeVal[0][1]);
 			$TypeResCheck=sortGDCodeArrays($TypeResCheck_T, 2 ,"true");
-			global $fpResData;
+			global $fpResData,$fpResDataT;
 			$fpResDataT=getMysqlDataArray($ResTable);
 			$fpResDataT2=filterArray($fpResDataT,0,$typeVal[0][1]);
 			$fpResData=sortGDCodeArrays($fpResDataT2 ,2,"true");
@@ -44,16 +79,64 @@
 			 $x=20;
 			 $y=60;
 			 $fontColor="#ffffff";
-			 sendVal_v2($URL, $sendarr,"check","xx",array(1522,0,46,18),10, $BgColor );
+		     //檢查按鈕
+			 $sendarr =array(  array("checks",$_POST["type"]))  ;
+			 sendVal_v2($URL, $sendarr,"check","xx",array(1122,0,46,18),10, $BgColor );
 			 for($i=0;$i<count($typeArray);$i++){
 				  $BgColor="#111111";
 			   if($typeVal[0][1]==$typeArray[$i])$BgColor="#aa1111";
 			      $sendarr =array( array("type",$typeArray[$i]))  ;
 				  sendVal_v2($URL, $sendarr,"check",$typeArray[$i],array($x+$i*50,$y,46,18),10, $BgColor );
-			 }				 
+			 }		
 	}
  ?>
- <?php
+ 
+<?php  //submit
+     function CheckSubmit(){
+	         if( $_POST["BaseSort"]=="")return;
+			 PregressUpdate();
+			   
+	 }
+     function PregressUpdate(){
+              global  $data_library,$tableName;
+			  global $URL;
+              global $ResTable ;
+		      $WHEREtable=array( "gdcode", "data_type" );
+		      $WHEREData=array( $_POST["code"],$_POST["type"]);
+			  echo ">>".$_POST["BaseSort"].">".$_POST["code"].">".$_POST["fin"];
+			  $Base=array( "checker");
+			  $nstr= reFinCode($_POST["BaseCode"],$_POST["BaseSort"],$_POST["fin"],$_POST["Checkcount"]);
+			  $up=array(  $nstr);
+			  $stmt= MakeUpdateStmt(  $data_library, $ResTable,$Base,$up,$WHEREtable,$WHEREData);
+			  SendCommand($stmt,$data_library);
+		      global $PostArray;
+			  $PostArray=array(array("type",$_POST["type"]));	
+			   ReLoad();
+	 } 
+     function ReLoad(){
+	    	   global $PostArray,$URL;
+			   JavaPost($PostArray,$URL); 
+	  }
+     function reFinCode($BaseFinCode,$sort,$fin,$Checkcount){
+		      echo "=".$Checkcount;
+	          $str=explode("_",$BaseFinCode); //根據空格切
+			  $nStr="";
+			  $f="1";
+			  if($fin=="1") $f="0";
+			  for($i=1;$i<=$Checkcount;$i++){
+				  $nStr =$nStr."_".$str[$i];
+			      if($i==($sort-2)){
+				     $nStr=$nStr."_".$f;
+				  }else{
+				  
+				  }
+			  }
+			  return $nStr;
+	 }
+
+?> 
+ 
+ <?php //列印
  
     function DrawList(){
 	         global $fpResData;
@@ -71,28 +154,8 @@
 	}
 	
 	
-	function DrawChecks($data, $y){
-	         global $TypeResCheck;
-			 global $URL;
-			 $fontSize=10;
-			 $fontColor="#ffffff";
-			 $x=120;
-			 $BgColor="#664400";
-			 $SubmitName="submit";
-			 $Ch=$data[15];
-			 $onOff=0;
-			 for($i=0;$i<count($TypeResCheck);$i++){
-				 
-				// $msg=$TypeResCheck[$i][1];
-				 $SubmitVal =$TypeResCheck[$i][1];
-			  	 $ValArray=array(array("code",$fpResData[$i][2]),
-				                 array("BaseSort",$TypeResCheck[$i][4]),
-								 array("OnOff", $onOff));
-			 	 $Rect=array($x,$y,50,20);
-			     sendVal_v2($URL,$ValArray,$SubmitName,$SubmitVal,$Rect,  $fontSize, $BgColor ,$fontColor );
-				 $x+=52;
-			 }
-	}
+
+	 
     function DrawChecker(){
 	         global $TypeResCheck;
 			 $fontSize=10;
@@ -106,4 +169,71 @@
 				 $x+=52;
 			 }
 	}
+	 function DrawChecks($data, $y){
+	         global $TypeResCheck;
+			 global $URL;
+			 $fontSize=10;
+			 $fontColor="#ffffff";
+			 $x=120;
+			 $BgColor="#664400";
+			 $SubmitName="submit";
+			 $Ch=$data[15];
+			 $onOff=0;
+			 for($i=0;$i<count($TypeResCheck);$i++){
+				 $BgColor="#666666";
+				 $SubmitVal =$TypeResCheck[$i][1];
+				 $fin=checkfin($data,$TypeResCheck[$i][4],$TypeResCheck[$i][5],$i);
+				 if($fin=="ResFin"){
+			    	$BgColor="#227722";
+					 DrawRect( $SubmitVal,$fontSize,$fontColor,$x,$y,50,20,$BgColor);
+				 }
+				 if($fin=="Res"){
+				    $BgColor="#555555";
+					DrawRect( $SubmitVal,$fontSize,$fontColor,$x,$y,50,20,$BgColor);
+				 }
+				 if($fin=="1" or $fin=="0"){
+					 if($fin=="1")$BgColor="#226622"; 
+			      	 $ValArray=array(array("code",$data [2]),
+					             array("type",$_POST["type"]),
+				                 array("BaseSort",$TypeResCheck[$i][4]),
+						         array("BaseCode",$data[15]),
+								 array("fin", $fin),
+							     array("Checkcount", count($TypeResCheck)) );
+			    	 $Rect=array($x,$y,50,20);
+			         sendVal_v2($URL,$ValArray,$SubmitName,$SubmitVal,$Rect,  $fontSize, $BgColor ,$fontColor );
+				 }
+				 $x+=52;
+			 }
+	}
+ ?>
+ 
+  <?php //檢查
+ 
+    function checkfin($data,$BaseSort,$ResSort,$sort){
+	         global $TypeResCheck;
+			 if($ResSort!=""){
+				 if(strpos($data[$ResSort],'已完成') !== false){ 
+			        return "ResFin";
+				 }else{
+				     return "Res";
+				 }
+			 }
+			 $str=explode("_",$data[15]); //根據空格切
+			 $t=$str[$sort];
+			 if($t=="")$t="0";
+			 return   $t;
+	}
+ 
+    function checkResData(){ //檢查英雄製作資料
+	        if ($_POST["checks"]=="")return;
+		        echo "xx".$_POST["checks"];
+		     global $ResTable ;
+	         $fpResDataT=getMysqlDataArray($ResTable);
+			 $fpResDataT2=filterArray($fpResDataT,0,$_POST["checks"]);
+			 $fpResData=sortGDCodeArrays($fpResDataT2 ,2,"true");
+		     for($i=0;$i<count($fpResDataT2);$i++){
+			     echo "</br>".$fpResDataT2[$i][3];
+			  }
+	}
+  
  ?>
